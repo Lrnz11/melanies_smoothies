3bb9a8d91ec9dba7b74950b8871f3611836b67ea
Lrnz11 <102259708+Lrnz11@users.noreply.github.com>
2025-12-27 21:03:25 +0000

    Remove commented-out dataframe display and stop; add search value display for selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,6 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(data=pd_df, use_container_width=True)
-st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:', 
@@ -55,6 +53,10 @@ if ingredients_list:
     # Example API call to Smoothiefroot for watermelon nutrition data   
     for fruit in ingredients_list:
         ingredients_string += fruit + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(f"Nutritional information for {fruit}")
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
